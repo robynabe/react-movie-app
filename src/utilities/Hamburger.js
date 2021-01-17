9e23b388fd43d427d6c81d3fa8a5b8a130dc08ad
robynabe <64428921+robynabe@users.noreply.github.com>
2021-01-17 13:56:27 -0800

    Hamburger icon added

--- src/utilities/Hamburger.js
+++ src/utilities/Hamburger.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+// This component toggles the mobile menu on and off
 export default class Hamburger extends Component {
+    // set the state to false so menu does not show
     state = {
         show: false,
     }
-
+    // made function toggle to corrrelate to the button, which makes show=true and displays the <ul>
     toggle = () => {
         this.setState({
             show: !this.state.show
@@ -14,6 +16,7 @@ export default class Hamburger extends Component {
     render() {
         return (
             <div>
+                <button className="hamburger-btn" onClick={this.toggle}><i class="fas fa-bars"></i></button>
                 {this.state.show && (
                     <ul className="mobile-nav">
                         <li><NavLink to={'/'}>Home</NavLink></li>
@@ -21,7 +24,6 @@ export default class Hamburger extends Component {
                         <li><NavLink to={'/favourites'}>Favourites</NavLink></li>
                     </ul>
                 )}
-                <button className="hamburger-btn" onClick={this.toggle}>Menu</button>
             </div>
         )
     }
