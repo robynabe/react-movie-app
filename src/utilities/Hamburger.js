10e2f9579611cbd76538293213e8165e3a2cfe61
robynabe <64428921+robynabe@users.noreply.github.com>
2021-01-17 14:54:16 -0800

    Implemented mobile menu close function

--- src/utilities/Hamburger.js
+++ src/utilities/Hamburger.js
@@ -13,15 +13,21 @@ export default class Hamburger extends Component {
             show: !this.state.show
         })
     }
+    // Closes the <ul> after menu item is clicked for new page
+    close = () => {
+        this.setState({
+            show: !this.state.show
+        })
+    }
     render() {
         return (
             <div className="mobile-nav">
                 <button className="hamburger-btn" onClick={this.toggle}><i class="fas fa-bars"></i></button>
                 {this.state.show && (
                     <ul>
-                        <li><NavLink to={'/'}>Home</NavLink></li>
-                        <li><NavLink to={'/about'}>About</NavLink></li>
-                        <li><NavLink to={'/favourites'}>Favourites</NavLink></li>
+                        <li><NavLink to={'/'} onClick={this.close}>Home</NavLink></li>
+                        <li><NavLink to={'/about'} onClick={this.close}>About</NavLink></li>
+                        <li><NavLink to={'/favourites'} onClick={this.close}>Favourites</NavLink></li>
                     </ul>
                 )}
             </div>
