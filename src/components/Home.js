543b51d2091b2a3ba7759d0c55887e3404e91b83
robynabe <64428921+robynabe@users.noreply.github.com>
2021-01-19 10:20:35 -0800

    Removed code from home into a component
    nested some scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,28 @@
 // HOME PAGE //
 import { useState, useEffect } from 'react';
-//import { getElementError } from "@testing-library/react"; @Olivia, did you add this? Robyn and I were trying to figure out what this was! 
+import MovieList from './Popular';
 
 function Home() {
   const API_KEY = '80b08b43125772f29e329b06bba72a9c';
-  //const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-  
-  const [popularMovies, setPopularMovies] = useState(null);
-  const fetchPopularMovies = async () =>{
-    const popular_movie_data = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-    );
-    const popular_movie_response = await popular_movie_data.json();
-    setPopularMovies(popular_movie_response.results);
-    console.log(popular_movie_response.results);
-  }
-  //fetchPopularMovies();
 
-  // const fetchMovieTitle = async () => {
-  //   const results = await fetch(API_URL);
-  //   let data = await results.json();
-  //   console.log(data.results); // Gives you all the info about every movie that starts with Soul
-  // }
+  const [movies, setMovies] = useState([]);
 
-  // fetchMovieTitle();
+  const getMovieRequest = async () => {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+    setMovies(responseJson.results);
+    }
+
+  // Request will get called only when the page loads
   useEffect(() => {
-    fetchPopularMovies();
-  } , [] )
+      getMovieRequest();
+   }, []);
 
-  let initialMovieArray = [];
+  /*let initialMovieArray = [];
   //let favouriteMovies = localStorage.getItem('favMovies');
   const movieObj = [];
   movieObj.title = 'Soul';
@@ -44,8 +38,7 @@ function Home() {
   
     //favMovies = JSON.stringify(favMovies);
     //localStorage.setItem('favMovies', favMovies);
-  }
-
+  }*/
 
   
   return (
@@ -57,41 +50,7 @@ function Home() {
             <option value="top-rated">Top Rated</option>
           </select>
         </form>
-        <section className="featured-movies">
-          <div className="movie-info">
-            <img className="movie-image" src="../images/soul-image-placeholder.jpg"/>
-            <div className="overview">
-              <h3>Soul</h3>
-              <p><i className="fas fa-star"></i> 4.0</p>
-              <h4>Overview</h4>
-              <p>Joe Gardner is a middle school teacher with a love for jazz music. After a successful gig at the Half Note Club, 
-                he suddenly... <a href="/">more info</a></p>
-                  <button className="far fa-heart favourite" onClick={addToFavourites}></button>
-            </div>
-          </div>
-          <div className="movie-info">
-            <img className="movie-image" src="../images/ww84-image-placeholder.jpg"/>
-            <div className="overview">
-              <h3>WW84</h3>
-              <p><i className="fas fa-star"></i> 5.0</p>
-              <h4>Overview</h4>
-              <p>Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable 
-                foe by... <a href="/">more info</a></p>
-                <button className="far fa-heart favourite" onClick={addToFavourites}></button>
-            </div>
-          </div>
-          <div className="movie-info">
-            <img className="movie-image" src="../images/cosmoball-image-placeholder.jpg"/>
-            <div className="overview">
-              <h3>Cosmoball</h3>
-              <p><i className="fas fa-star"></i> 3.0</p>
-              <h4>Overview</h4>
-              <p>Cosmoball is a mesmerizing intergalactic game of future played between humans and aliens at the giant extraterrestrial 
-                ship... <a href="/">more info</a></p>
-                <button className="far fa-heart favourite" onClick={addToFavourites}></button>
-            </div>
-          </div>
-        </section>
+            <MovieList movies={movies} />
     </main>
   );
 }
