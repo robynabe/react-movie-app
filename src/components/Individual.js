21dd22734689a5e0791ac0f332ce7f5bdad96c70
omcdanielfeilden <omcdanielfeilden@gmail.com>
2021-01-26 21:55:41 -0800

    .

--- src/components/Individual.js
+++ src/components/Individual.js
@@ -7,56 +7,64 @@ import saveToLocalStorage from './Home';
 
 function Individual() {
     const API_KEY = '80b08b43125772f29e329b06bba72a9c';
-    const { id } = useParams();
-    const [movie, setMovie] = useState(null);
+        const { id } = useParams();
+            const [movie, setMovie] = useState(null);
+                useEffect( () => {
+                    const getSingleMovie = async () => {
+                        const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=credits`);
+                    let data = await response.json();
+                setMovie(data);
+            console.log(data.genres);};
+        getSingleMovie();},[id]);
 
-    useEffect( () => {
-            const getSingleMovie = async () => {
-            const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=credits`);
-            // https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US
-            let data = await response.json();
-            setMovie(data);
-            console.log(data.genres);
-        };
-        getSingleMovie();
-    }, [id]);
+// Make Cast function ??
+    function makeCast(movie){ let top5Cast = movie.credits.cast.filter(person => person.order < 5);
+        top5Cast.sort((a,b) => a.order - b.order);
+            top5Cast.map((person, index) => {
+                return (
+            <p key={index}>{person.name}</p>
+          );
+        }); }
 
-    return (
-
-        <main>
-            <button className="return-home-btn">
-            <Link to={'/'}><i className="fas fa-step-backward"></i></Link>
-            </button>
-                {movie !== null &&
-                    <div className="movie-poster-individual">
-                        <img className="single-movie-image" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}/>
-                            <AddFavourites handleFavouritesClick={AddFavouriteMovie, saveToLocalStorage} />
-                        <div className="movie-info-individual">
-                            <h2>{movie.title}</h2>
+        return (
+            <main>
+                <button className="return-home-btn">
+                <Link to={'/'}><i className="fas fa-step-backward"></i></Link>
+                </button>
+                    {movie !== null &&
+                        <div className="movie-poster-individual">
+                            <img className="single-movie-image" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}/>
+                                <AddFavourites handleFavouritesClick={AddFavouriteMovie, saveToLocalStorage} />
+                            <div className="movie-info-individual">
+                        <h2>{movie.title}</h2>
                             <h3>{movie.release_date}</h3>
-                            {
-                            movie.genres.map((genre) =>
-                                <li key={genre.name}>{genre.name}</li> // we can change the syntax here when we know how we want to style!
-                            )}
-                            <h5>{movie.runtime} min</h5>
-                        </div>
-                        <p><i className="fas fa-star"></i> {movie.vote_average}</p>
-                    <h4>Synopsis</h4>
-                    <p>{movie.overview}minutes</p>
-                        <h4>Cast</h4>
-                        {movie.credits.cast.map((person, index) => (
-                            <p>{person.name}</p>
-                         ))}
-                      
+                                {movie.genres.map((genre) =>
+                                    <li key={genre.name}>{genre.name}</li>)}
+                                <h5>{movie.runtime} min</h5>
+                            </div>
+                            <p><i className="fas fa-star"></i> {movie.vote_average}</p>
+                        <h4>Synopsis</h4>
+                            <p>{movie.overview}minutes</p>
+                                <h4>Cast</h4>
+                            { makeCast(movie) }
+
+                        {/* {Make Cast Map} */}
+                        {/* {movie.credits.cast.map((person, index) => {
+                        if(person.order < 5){
+                        return <p key={index}>{person.name}</p>}})} */}
 
                         <h4>Producers</h4>
-                    <p>Kate Croser, Jason Taylor, Lisa Shaunessy</p>
-                <h4>Director(s)</h4>
-                    <p>Seth Larney</p>
-                </div>
-                }
-        </main>
-    );
-}
+                            {movie.credits.crew.map((person, index) => {
+                                <p>{person.name}</p>})}
+                                    <h4>Director(s)</h4>
+                                {movie.credits.crew.map((person, index) => {
+                            <p>{person.name}</p>})}
+                    </div>
+                    }
+            </main>
 
-export default Individual;
+            
+        );
+    }
+
+    export default Individual;
