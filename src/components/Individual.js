5dd4a3dd33ee82b01be5304561e86d4f0f74bca5
Seraphina White <seraphinawhite.6@gmail.com>
2021-01-28 15:25:36 -0800

    Individual Adjustments

--- src/components/Individual.js
+++ src/components/Individual.js
@@ -12,7 +12,7 @@ function Individual(props) {
     const API_KEY = '80b08b43125772f29e329b06bba72a9c';
 
     const [favourites, setFavourites] = useState([]);
-    const FavouriteComponent = props.favouriteComponent;
+    //const FavouriteComponent = props.favouriteComponent;
 
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
@@ -34,18 +34,6 @@ function Individual(props) {
             <p key={index}>{person.name}</p>
           );
         }); }
-
-        /*
-        useEffect(() => {
-            let movieFavourites = localStorage.getItem('favourites')
-            if (movieFavourites === null){
-              movieFavourites = [];
-            }else{
-              movieFavourites = JSON.parse(movieFavourites);
-            }
-            setFavourites(movieFavourites);
-         },[]);
-         */
     
         const saveToLocalStorage = (items) => {
           localStorage.setItem('favourites', JSON.stringify(items))
@@ -53,7 +41,6 @@ function Individual(props) {
 
         // Trying to stop duplicate movies added to faves 
             const AddFavouriteMovie = (movie) => {
-            // if(useState.movieFavourites.some((movie) => movie.id === movie.id)){
             const newFavouriteList = [...favourites, movie]
             setFavourites(newFavouriteList);
             saveToLocalStorage(newFavouriteList);
@@ -85,72 +72,60 @@ function Individual(props) {
                 return producer.map(producer => <p>{producer}</p>);
                 }
             }
-        return (
-            <main>
-                <div className="vl-left"></div>
-                <div className="vl-right"></div>
-                {movie !== null &&
-                    <div class="movie-individual-container">
-                        {/* Movie Poster Div */}
-                        
-                            <div className="movie-poster-individual">
-                                <img className="single-movie-image" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}/>
-                                <h1 className="individual-movie-title">{movie.title}</h1>
-                                {/* <img className="single-movie-image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/> */}
-                            </div>
-                        <div className="movie-individual-wrapper">
-                            <div className="movie-info-individual">
-                                <div className="rating-release">
-                                    <p className="release-date">{movie.release_date}</p>
-                                    <div className="rate-heart">
-                                        <p><i className="fas fa-star"></i> {movie.vote_average}</p>
-                        
-                                            <RemoveFavourites movie={movie} className="fav-btn-individual" handleAddFavourites={AddFavouriteMovie} handleDeleteFavourites={deleteFavourites}/>
-                                    </div>
+    return (
+        <main>
+            <div className="vl-left"></div>
+            <div className="vl-right"></div>
+            {movie !== null &&
+                <div class="movie-individual-container">
+                        <div className="movie-poster-individual">
+                            <img className="single-movie-image" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}/>
+                            <h1 className="individual-movie-title">{movie.title}</h1>
+                        </div>
+                    <div className="movie-individual-wrapper">
+                        <div className="movie-info-individual">
+                            <div className="rating-release">
+                                <p className="release-date">{movie.release_date}</p>
+                                <div className="rate-heart">
+                                    <p><i className="fas fa-star"></i> {movie.vote_average}</p>
+                                        <RemoveFavourites movie={movie} className="fav-btn-individual" handleAddFavourites={AddFavouriteMovie} handleDeleteFavourites={deleteFavourites}/>
                                 </div>
-                                <div className="small-container">
-                                    
-                                        <h5>Synopsis</h5>
-                                        <p>{movie.overview}</p>
-                                        <h6>Cast</h6>
-                                        <ul className="cast-list">
-                                            {movie.credits.cast.map((person, index) => {
-                                                if(person.order < 5){
-                                                return <li key={index} className="list-item">{person.name}</li> 
-                                                }
-                                            })} 
-                                        </ul>
-                                    <h6>Producers</h6>
-                                    <div class="producer-list">
-                                        {getProducer(movie)}
-                                    </div>
-                                    <h6>Director</h6>
-                                    <div className="director-list">
-                                        {getDirectors(movie)}
-                                    </div>
-                                    <hr></hr>
-                                    <div className="genre-run-time">
-                                        <ul className="genre-list">
-                                            {movie.genres.map((genre) =>
-                                            <li key={genre.name} className="list-item">{genre.name}</li>)}
-                                        </ul>
-                                        <p>{movie.runtime} min</p>
-                                    </div>
+                            </div>
+                            <div className="small-container">
+                                    <h5>Synopsis</h5>
+                                    <p>{movie.overview}</p>
+                                    <h6>Cast</h6>
+                                    <ul className="cast-list">
+                                        {movie.credits.cast.map((person, index) => {
+                                            if(person.order < 5){
+                                            return <li key={index} className="list-item">{person.name}</li> 
+                                            }
+                                        })} 
+                                    </ul>
+                                <h6>Producers</h6>
+                                <div class="producer-list">
+                                    {getProducer(movie)}
+                                </div>
+                                <h6>Director</h6>
+                                <div className="director-list">
+                                    {getDirectors(movie)}
+                                </div>
+                                <hr></hr>
+                                <div className="genre-run-time">
+                                    <ul className="genre-list">
+                                        {movie.genres.map((genre) =>
+                                        <li key={genre.name} className="list-item">{genre.name}</li>)}
+                                    </ul>
+                                    <p>{movie.runtime} min</p>
+                                </div>
 
-                                </div>
-                                
-                                    
-                                
-                            </div>
+                            </div> 
                         </div>
                     </div>
-                }
-                  {/* <button onClick={()=> AddFavouriteMovie(movie)} className="fav-btn">
-                      <FavouriteComponent handleFavouritesClick={AddFavouriteMovie} favouriteComponent = {AddFavourites} />
-                      </button>
-                  */}   
-                  <Scroll />  
-            </main>
-        );
+                </div>
+            }  
+                <Scroll />  
+        </main>
+    );
     }
 export default Individual;
