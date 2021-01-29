4b9a0232e9c722c99b61763914bb99f8338d26fc
robynabe <64428921+robynabe@users.noreply.github.com>
2021-01-29 09:50:38 -0800

    Fixed sub nav buttons

--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 function SubNav() {
   return (
     <nav className="sub-nav">
-          <button className="select-btn"><NavLink to={'/'} sort={'popular'}>Popular</NavLink></button>
-          <button className="select-btn"><NavLink to={'/sort/now-playing'} sort={'now_playing'}>Now Playing</NavLink></button>
-          <button className="select-btn"><NavLink to={'/sort/top-rated'} sort={'top_rated'}>Top Rated</NavLink></button>
-          <button className="select-btn"><NavLink to={'/sort/upcoming'} sort={'upcoming'}>Upcoming</NavLink></button>
+          <NavLink to={'/'} sort={'popular'}><button className="select-btn">Popular</button></NavLink>
+          <NavLink to={'/sort/now-playing'} sort={'now_playing'}><button className="select-btn">Now Playing</button></NavLink>
+          <NavLink to={'/sort/top-rated'} sort={'top_rated'}><button className="select-btn">Top Rated</button></NavLink>
+          <NavLink to={'/sort/upcoming'} sort={'upcoming'}><button className="select-btn">Upcoming</button></NavLink>
     </nav>
   );
 }
